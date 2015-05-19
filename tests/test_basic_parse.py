94740161de38ef44b8b750b1b4cd88a62809a684
James Johnson <james@narly.me>
2015-05-19 07:11:23 -0500

    relative path in test script

--- tests/test_basic_parse.py
+++ tests/test_basic_parse.py
@@ -8,7 +8,7 @@ sys.path.insert(0, "..")
 from py010parser import parse_file, parse_string, c_ast
 
 def template_path(template_name):
-    return os.path.join("templates", template_name)
+    return os.path.join(os.path.dirname(__file__), "templates", template_name)
 
 class TestBasicParse(unittest.TestCase):
     def setUp(self):
