751c99800eaaf45b15ba8827ca6801e19f3a70b4
James Johnson <d0c.s4vage@gmail.com>
2015-08-31 07:06:40 -0500

    version bump

--- py010parser/__init__.py
+++ py010parser/__init__.py
@@ -8,7 +8,7 @@
 # License: BSD
 #-----------------------------------------------------------------
 __all__ = ['c_lexer', 'c_parser', 'c_ast']
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 from subprocess import Popen, PIPE
 from .c_parser import CParser
